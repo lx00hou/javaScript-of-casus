ed4b26e0dbc5261719defd06647295496c3427a0
铁蛋 <1132125385@qq.com>
2022-11-27 07:04:11 +0800

    歌词滚动效果 开发完成

--- 歌词滚动效果/js/index.js
+++ 歌词滚动效果/js/index.js
@@ -4,7 +4,9 @@
  * @param {HTMLAudioElement}  音频播放器 
  */
 let dom = {
-    audio:document.querySelector('audio')
+    audio:document.querySelector('audio'),
+    contain:document.querySelector('section'),
+    Ul:document.querySelector('ul')
 }
 
 
@@ -20,6 +22,7 @@ let dom = {
     },
     curLrcIndex:() => {
         let curAudioTime = dom.audio.currentTime;
+        // let curAudioTime = 67.76;
         for(var i = 0 ; i <lrcData.length; i++ ){
             if(curAudioTime < lrcData[i].time){
                  return i - 1 
@@ -29,6 +32,9 @@ let dom = {
         return lrcData.length - 1
     }
 }
+
+
+
 function processData(lyric){
     /**
      * 处理拿到的数据格式
@@ -47,7 +53,55 @@ function processData(lyric){
 var lrcData = processData(lrc) || [];
 console.log('Index',unit.curLrcIndex());
 
+var containHeight = dom.contain.clientHeight;   // 可视容器 高度
 
+
+function createLrcItem(){
+    /**
+     * 循环数据 渲染 li 标签
+     */
+    let frag = document.createDocumentFragment();
+    lrcData.forEach(lrc => {
+        let li = document.createElement('li');
+        li.textContent = lrc.words;
+        frag.appendChild(li);
+    })
+    dom.Ul.appendChild(frag);
+    setUlPosition();
+}
+createLrcItem();
+
+
+
+function setUlPosition(){
+    /**
+     * @param {number} 32 单个 li 高度
+     * @param {number} 16 单个 li 高度的一般
+     * 实际开发中建议动态获取
+     */
+    let InvolvedHeight = unit.curLrcIndex() * 32;
+    let upTranslateY =  InvolvedHeight + 16 - ( containHeight / 2 ) 
+    if(upTranslateY < 0) upTranslateY = 0;
+
+    /**
+     * 通过 transform 设置 Ul 整体的偏移量 
+     */
+    dom.Ul.style.transform = `translateY(-${upTranslateY}px)`;
+
+    /**
+     * 判断是否存在 li 添加类名
+     */
+    let li = document.querySelector('.active');
+    li && li.classList.remove('active');
+    li = dom.Ul.children[unit.curLrcIndex()];
+    li && li.classList.add('active');
+
+}
+
+/**
+ * 播放器 时间更新 
+ */
+dom.audio.addEventListener('timeupdate',setUlPosition)
 
 
 
