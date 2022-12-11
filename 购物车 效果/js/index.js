4e9e79f38c8f153557eda3c6be70f58a286734d4
铁蛋 <1132125385@qq.com>
2022-12-11 18:11:19 +0800

    demo 初始化 && 创建 class 商品类

---
+++ 购物车 效果/js/index.js
@@ -0,0 +1,66 @@
+// 单个商品 信息集合
+class UiGoods {
+    constructor(goods){
+        this.data = goods,
+        this.choose = 0
+    }
+    // 获取单个商品的总价
+    getItemsTotalPrice(){
+        return this.data.price * this.choose 
+    }
+    // 是否选中
+    isChoose(){
+        return this.choose ? true : false
+    }
+    // 选择数量+1
+    increase(){
+        this.choose++
+    }
+    // 选择数量-1
+    desrease(){
+        if(!this.choose) return
+        this.choose--
+    }
+}
+
+
+class UiData{
+    constructor(){
+        let uiGoods = [];
+        goods.forEach(item => {
+            uiGoods.push(new UiGoods(item))
+        })
+        this.goodsData = uiGoods    // 获取页面所有商品相关数据 
+        this.deliverThreshold = 100    // 起送费
+        this.deliverPrice = 5 // 配送费
+    }
+    //  获取 当前 选中的的商品总价
+    getTotalPrice(){
+        return this.goodsData.reduce((total,next) => {
+             total += next.getItemsTotalPrice();
+             return total  
+        },0)
+    }
+
+    // 获取当前 选中 商品总数量
+    getTotalChooseNum(){
+    return this.goodsData.reduce((total,next) => {
+        total += next.choose;
+        return total 
+    },0)
+    }
+
+    // 根据下标 增加某件商品的 数量
+    increase(index){
+        this.goodsData[index].increase()
+    }
+
+    // 根据下标 减少某件商品的 数量
+    desrease(index){
+        this.goodsData[index].desrease()
+    }
+
+ 
+}
+
+let datas = new UiData() 
