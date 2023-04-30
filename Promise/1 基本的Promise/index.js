e60e6aca2f46de6b3e706a522696ed1803c6f006
铁蛋 <1132125385@qq.com>
2023-04-30 13:41:26 +0800

    处理Promise 异步声明问题

--- Promise/1 基本的Promise/index.js
+++ Promise/1 基本的Promise/index.js
@@ -2,16 +2,19 @@
  * Promise 特点
  * 三种状态 等待(默认) 成功 失败,一旦成功就不能失败 反之一样,同时 无法终止
  * 每个Promise实例都有then 方法
- * new Promise 报错 会变成失败状态
+ * new Promise时 报错 会直接变成失败状态
  */
 
-let Promise = require('./Promise');
+
+let Promise = require('./promise');
 let promise = new Promise((resolve,reject) => {   // 参数是一个函数 executor 执行器 (立即执行)
     /**
      * 同步执行
      * throw new Error('失败') 报错会直接变为失败状态
      *  */ 
-    resolve('suc');
+    setTimeout(() => {
+        resolve('suc');
+    },1000)
 }).then((data) => {
     console.log('成功',data);
 },err => {
--- Promise/1 基本的Promise/promise.js
+++ Promise/1 基本的Promise/promise.js
@@ -5,9 +5,18 @@ const REJECTED = 'REJECTED';
 class Promise{
     constructor(executor){
         this.status = PENDING;    // 默认pending状态
-        this.value = undefined;   // 成功的值
+        this.value = undefined;   // 成功的值(实例上)
         this.reason = undefined;  // 失败的原因
-        let resolve = (val) => {
+        /**
+         * 异步调用
+         * 只有当resolve 或者 reject 被异步执行时 then方法时
+         * status 是pending 状态, 需要订阅 发布
+         * onResolvedCb: 成功的 回调数组
+         * onRejectedCb: 失败的 回调数组
+         */
+        this.onResolvedCb = [];
+        this.onRejectedCb = [];
+        let resolve = (val) => {   // 私有
             /**
              * 成功函数 
              * 只有当前是等待状态 才执行
@@ -16,6 +25,8 @@ class Promise{
             if(this.status === PENDING){
                 this.value = val;
                 this.status = RESOLVED;
+                // 处理异步
+                this.onResolvedCb.length && this.onResolvedCb.forEach(fn => fn())  //发布
             }
 
         }
@@ -26,27 +37,39 @@ class Promise{
              * 更改状态 赋值失败原因
              */
             if(this.status === PENDING){
-                this.reason = reason
+                this.reason = reason;
                 this.status = REJECTED;
+                // 处理异步
+                this.onRejectedCb.length && this.onRejectedCb.forEach(fn => fn())   // 发布
             }
 
         }
         try {
             // 报错 直接走失败状态
             executor(resolve,reject)    // 执行器立即执行
-        } catch (error) {
-            reject(error);
-        }
+        } catch (error) { reject(error) }
     }
-
+    // 原型then方法 
     then(onfulfilled,onrejected){
+        // 同步调用
         if(this.status === RESOLVED){
             onfulfilled(this.value)
         }
         if(this.status === REJECTED){
             onrejected(this.reason)
         }
+        // 异步调用 此时状态时 'PENDING'  需要借助发布订阅
+        if(this.status === PENDING){   // 订阅
+            this.onResolvedCb.push(() => {    
+                onfulfilled(this.value)
+            });
+            this.onRejectedCb.push(() => {
+                onrejected(this.reason)
+            })
+        }
 
     }
+    
 }
+
 module.exports = Promise 
